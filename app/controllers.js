1ae8479a9808050354cf4334e64e6951762c3f8e
Michael Crosby <michael@crosbymichael.com>
2014-04-27 11:22:04 -0700

    Update start with cpu, memory, and scale

--- app/controllers.js
+++ app/controllers.js
@@ -49,17 +49,61 @@ function ServicesController($scope) {
 function StartController($scope) {
     $scope.template = 'partials/start.html';
 
+    var maxHosts = 202;
+
+    $scope.scale = 1;
+    $scope.cpuShares = 100;
+    $scope.memory = 52;
+    $scope.image = '';
+    $scope.cores = 1;
+
     $scope.images = [
         'redis',
         'rethinkdb',
         'nsqd',
         'nsqadmin'
     ];
+
+    $scope.coresUp = function () {
+        $scope.cores++;
+    };
+    $scope.coresDown = function () {
+        if ($scope.cores > 1) {
+            $scope.cores--;
+        }
+    };
+    $scope.scaleUp = function () {
+        if ($scope.scale < maxHosts) {
+            $scope.scale++;
+        }
+    };
+    $scope.scaleDown = function () {
+        if ($scope.scale > 1) {
+            $scope.scale--;
+        }
+    };
+
+    $scope.launchContainers = function () {
+        // have to get the value this way because binding to a checkbox
+        // is near impossible or I am doing something wrong
+        var isService = $('#is-service-checkbox')[0].checked
+        console.log(isService);
+    };
 }
 
+// this needs to move to some super start init func
 function toggleStartSidebar() {
     $('.ui.sidebar').sidebar({
         overlay: true
     })
         .sidebar('toggle');
+
+    $('.ui.dropdown')
+        .dropdown();
+
+    $('.ui.checkbox')
+        .checkbox();
+
+    $('.ui.radio.checkbox')
+        .checkbox();
 }
