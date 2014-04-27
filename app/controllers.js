877a3ef0ed54280513ded00431f1b7c3eb86d629
Michael Crosby <michael@crosbymichael.com>
2014-04-26 23:28:48 -0700

    Update header with hosts and container count

--- app/controllers.js
+++ app/controllers.js
@@ -2,8 +2,15 @@
 
 function HeaderController($scope) {
     $scope.template = 'partials/header.html';
+
+    $scope.hosts = 202;
+    $scope.runningContainers = 17338;
 }
 
 function DashboardController($scope) {
+    $scope.template = 'partials/dashboard.html';
 
+    $scope.cpuUsage = 48;
+    $scope.memoryUsage = 65;
+    $scope.diskUsage = 20;
 }
