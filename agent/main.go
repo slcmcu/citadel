48e5ce80bb47841cd00c9f73695feb438d53120a
Michael Crosby <michael@crosbymichael.com>
2014-05-06 22:37:38 -0700

    Update management ui to use gorilla mux

--- agent/main.go
+++ agent/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"flag"
-	"net"
-	"os"
-	"os/signal"
-	"runtime"
+	"net/http"
 	"strings"
 	"time"
 
@@ -15,6 +10,7 @@ import (
 	"citadelapp.io/citadel/metrics"
 	"citadelapp.io/citadel/repository"
 	"github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
 )
 
 var (
@@ -23,72 +19,37 @@ var (
 	log      = logrus.New()
 )
 
-// getAgentName gets the agent name based upon the first available mac address
-func getAgentName() string {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		log.Fatalf("Unable to detect agent name: %s", err)
-	}
-	name := "unknown"
-	for _, iface := range ifaces {
-		n := iface.HardwareAddr.String()
-		if n != "" {
-			name = strings.Replace(n, ":", "", -1)
-			break
-		}
-	}
-	return name
-}
-
-func generateHostId(name string) string {
-	h := sha1.New()
-	h.Write([]byte(name))
-	return hex.EncodeToString(h.Sum(nil))
-}
-
-func getHostInfo(name string) (*citadel.Host, error) {
-	cpus := runtime.NumCPU()
-	memUsage, err := getMemoryUsage()
-	if err != nil {
-		return nil, err
-	}
-
-	diskUsage, err := getDiskUsage()
-	if err != nil {
-		return nil, err
-	}
-
-	host := &citadel.Host{
-		Name:        name,
-		IPAddress:   listen,
-		Cpus:        cpus,
-		TotalMemory: memUsage.Total,
-		Disks:       diskUsage,
-	}
-
-	return host, nil
-}
-
 func init() {
 	flag.StringVar(&listen, "listen", "", "Listen address")
 	flag.StringVar(&machines, "machines", "127.0.0.1:4001", "Comma separated list of etcd machines")
 	flag.Parse()
 }
 
+func collectMetrics(store metrics.Store, host *citadel.Host, conf *citadel.Config) {
+	for _ = range time.Tick(time.Duration(conf.PullInterval) * time.Second) {
+		if err := pushHostMetrics(host, store); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
 func main() {
 	if listen == "" {
 		log.Fatal("You must specify a listen address")
 	}
-	etcdMachines := strings.Split(machines, ",")
-	repo := repository.NewEtcdRepository(etcdMachines)
+	var (
+		etcdMachines = strings.Split(machines, ",")
+		repo         = repository.NewEtcdRepository(etcdMachines)
+	)
 
 	conf, err := repo.FetchConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	agentName := getAgentName()
-
+	agentName, err := getAgentName()
+	if err != nil {
+		log.Fatal(err)
+	}
 	host, err := getHostInfo(agentName)
 	if err != nil {
 		log.Fatal(err)
@@ -98,6 +59,7 @@ func main() {
 	if err := repo.SaveHost(host); err != nil {
 		log.Fatalf("Unable to save host: %s", err)
 	}
+
 	log.WithFields(logrus.Fields{
 		"cpus":      host.Cpus,
 		"memory":    host.TotalMemory,
@@ -114,21 +76,10 @@ func main() {
 		"address":  listen,
 	}).Info("Citadel Agent")
 
-	var (
-		sig             = make(chan os.Signal)
-		hostMetricsTick = time.Tick(time.Duration(conf.PullInterval) * time.Second)
-	)
-	signal.Notify(sig, os.Interrupt)
+	go collectMetrics(store, host, conf)
 
-	for {
-		select {
-		case <-hostMetricsTick:
-			if err := pushHostMetrics(host, store); err != nil {
-				log.Fatal(err)
-			}
-		case <-sig:
-			log.Info("Shutting down Citadel")
-			return
-		}
+	r := mux.NewRouter()
+	if err := http.ListenAndServe(listen, r); err != nil {
+		log.Fatal(err)
 	}
 }
--- management/main.go
+++ management/main.go
@@ -1,69 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"net/http"
 	"strings"
 
 	"citadelapp.io/citadel/metrics"
 	"citadelapp.io/citadel/repository"
 	"github.com/Sirupsen/logrus"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
+	"github.com/gorilla/mux"
 )
 
 var (
 	machines string
-	log      = logrus.New()
+	assets   string
+
+	store metrics.Store
+	repo  repository.Repository
+	log   = logrus.New()
 )
 
 func init() {
 	flag.StringVar(&machines, "machines", "127.0.0.1:4001", "Comma separated list of etcd machines")
+	flag.StringVar(&assets, "assets", "management", "Path the the http assets")
 	flag.Parse()
 }
 
-func main() {
-	m := martini.Classic()
-	m.Use(render.Renderer())
-
-	etcdMachines := strings.Split(machines, ",")
-	repo := repository.NewEtcdRepository(etcdMachines)
-	conf, err := repo.FetchConfig()
-	if err != nil {
-		log.Fatalf("Unable to get config from etcd: %s", err)
-	}
-	store, err := metrics.NewStore(conf)
+func getContainers(w http.ResponseWriter, r *http.Request) {
+	data, err := repo.FetchContainerGroup()
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	m.Group("/api", func(r martini.Router) {
-		r.Get("/hosts", func(rdr render.Render) {
-			data, err := repo.FetchHosts()
-			if err != nil {
-				log.Fatal(err)
-			}
-			rdr.JSON(200, data)
-		})
+func getHosts(w http.ResponseWriter, r *http.Request) {
+	data, err := repo.FetchHosts()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
 
-		r.Get("/containers", func(rdr render.Render) {
-			data, err := repo.FetchContainerGroup()
-			if err != nil {
-				log.Fatal(err)
-			}
-			rdr.JSON(200, data)
-		})
+func getHostMetrics(w http.ResponseWriter, r *http.Request) {
+	var (
+		params = mux.Vars(r)
+		table  = fmt.Sprintf("metrics.host.%s", params["name"])
+	)
 
-		r.Get("/hosts/:name/metrics", func(params martini.Params, rdr render.Render) {
-			table := fmt.Sprintf("metrics.host.%s", params["name"])
-			data, err := store.Fetch(fmt.Sprintf("select * from %s group by time(5m) where time > now() -12h limit 200", table))
-			if err != nil {
-				log.Fatal(err)
-			}
-			rdr.JSON(200, data)
-		})
-	})
+	data, err := store.Fetch(fmt.Sprintf("select * from %s group by time(5m) where time > now() -12h limit 200", table))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	m.Use(martini.Static("."))
-	m.Run()
+func main() {
+	var (
+		err          error
+		globalMux    = http.NewServeMux()
+		apiRouter    = mux.NewRouter()
+		etcdMachines = strings.Split(machines, ",")
+	)
+
+	repo = repository.NewEtcdRepository(etcdMachines)
+	conf, err := repo.FetchConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if store, err = metrics.NewStore(conf); err != nil {
+		log.Fatal(err)
+	}
+
+	apiRouter.HandleFunc("/api/containers", getContainers)
+	apiRouter.HandleFunc("/api/hosts", getHosts)
+	apiRouter.HandleFunc("/api/hosts/{name:.*}/metrics", getHostMetrics)
+
+	globalMux.Handle("/api/", apiRouter)
+	globalMux.Handle("/", http.FileServer(http.Dir(assets)))
+
+	if err := http.ListenAndServe(":3000", globalMux); err != nil {
+		log.Fatal(err)
+	}
 }
