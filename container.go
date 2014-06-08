606081db31a1c188157c0729f95b5de4e3d84bd2
Michael Crosby <michael@crosbymichael.com>
2014-06-08 11:15:22 -0700

    Update with container state

--- container.go
+++ container.go
@@ -2,6 +2,27 @@ package citadel
 
 import "time"
 
+// Status of the container
+type Status string
+
+const (
+	Created Status = "created"
+	Running Status = "running"
+	Stopped Status = "stopped"
+)
+
+// State represents the containers state
+type State struct {
+	// The current status of the container
+	Status Status
+	// StartedAt is the time when the container was launched
+	StartedAt time.Time
+	// ExitedAt is the time the container exited
+	ExitedAt time.Time
+	// ExitCode is the code which the container last returned on exit
+	ExitCode int
+}
+
 // Container represents a running container on the cluster
 type Container struct {
 	// ID is a unique identifier for a container
@@ -14,6 +35,6 @@ type Container struct {
 	Cpus int
 	// Memory is the amount of memory in mb that the container has reserved
 	Memory int
-	// StartedAt is the time when the container was launched
-	StartedAt time.Time
+	// State is the current state of the container
+	State State
 }
