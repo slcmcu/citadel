a6c2e7fbc62887bcef4a5b3cc75fb771c7b4a4d7
Michael Crosby <michael@docker.com>
2014-07-12 09:45:37 -0700

    Use toml config file instead of flags

--- citadel/host.go
+++ citadel/host.go
@@ -7,7 +7,6 @@ import (
 	"syscall"
 
 	"github.com/citadel/citadel"
-	"github.com/citadel/citadel/utils"
 	"github.com/codegangsta/cli"
 	"github.com/samalba/dockerclient"
 )
@@ -17,27 +16,25 @@ var hostCommand = cli.Command{
 	Usage:  "run the host and connect it to the cluster",
 	Action: hostAction,
 	Flags: []cli.Flag{
-		cli.StringFlag{"addr", "", "external ip address for the host"},
-		cli.StringFlag{"docker", "unix:///var/run/docker.sock", "docker remote ip address"},
-		cli.IntFlag{"cpus", -1, "number of cpus available to the host"},
-		cli.IntFlag{"memory", -1, "number of mb of memory available to the host"},
-		cli.StringFlag{"listen", ":8787", "listen address"},
-		cli.StringSliceFlag{"labels", &cli.StringSlice{}, "labels to apply as attributes of the host"},
+		cli.StringFlag{"config", "host.toml", "config for the host"},
 	},
 }
 
 func hostAction(context *cli.Context) {
-	validateContext(context)
+	config, err := loadConfig(context.String("config"))
+	if err != nil {
+		logger.WithField("error", err).Fatal("load config")
+	}
 
-	host, err := citadel.NewHost(getHostId(), context.Int("cpus"), context.Int("memory"), context.StringSlice("labels"), getClient(context), logger)
+	host, err := citadel.NewHost(config.ID, config.Cpus, config.Memory, config.Labels, getClient(config), logger)
 	if err != nil {
 		logger.WithField("error", err).Fatal("create host")
 	}
 
 	server := citadel.NewServer(host)
-
 	go waitForInterrupt()
-	if err := http.ListenAndServe(context.String("addr"), server); err != nil {
+
+	if err := http.ListenAndServe(config.Addr, server); err != nil {
 		logger.WithField("error", err).Fatal("listen and serve")
 	}
 }
@@ -51,30 +48,11 @@ func waitForInterrupt() {
 	}
 }
 
-func getHostId() string {
-	id, err := utils.GetMachineID()
-	if err != nil {
-		logger.WithField("error", err).Fatal("unable to read machine id")
-	}
-	return id
-}
-
-func getClient(context *cli.Context) *dockerclient.DockerClient {
-	client, err := dockerclient.NewDockerClient(context.String("docker"))
+func getClient(config *Config) *dockerclient.DockerClient {
+	client, err := dockerclient.NewDockerClient(config.Docker)
 	if err != nil {
 		logger.WithField("error", err).Fatal("unable to connect to docker")
 	}
 
 	return client
-}
-
-func validateContext(context *cli.Context) {
-	switch {
-	case context.Int("cpus") < 1:
-		logger.Fatal("cpus must have a value")
-	case context.Int("memory") < 1:
-		logger.Fatal("memory must have a value")
-	case context.String("addr") == "":
-		logger.Fatal("addr must have a value")
-	}
 }
