031e708c749dfb60611d3dfd37b3f3faca72ba0b
Michael Crosby <michael@docker.com>
2014-07-13 20:53:54 -0700

    Add ability to interact with multiple hosts

--- citadel/actions.go
+++ citadel/actions.go
@@ -27,6 +27,9 @@ var (
 		Name:   "delete",
 		Usage:  "delete an application and all containers",
 		Action: deleteAction,
+		Flags: []cli.Flag{
+			cli.StringSliceFlag{"hosts", &cli.StringSlice{}, "hosts to load the app on"},
+		},
 	}
 
 	loadCommand = cli.Command{
@@ -42,35 +45,41 @@ var (
 		Name:   "start",
 		Usage:  "start an application",
 		Action: startAction,
+		Flags: []cli.Flag{
+			cli.StringSliceFlag{"hosts", &cli.StringSlice{}, "hosts to load the app on"},
+		},
 	}
 
 	stopCommand = cli.Command{
 		Name:   "stop",
 		Usage:  "stop an application",
 		Action: stopAction,
+		Flags: []cli.Flag{
+			cli.StringSliceFlag{"hosts", &cli.StringSlice{}, "hosts to load the app on"},
+		},
 	}
 )
 
 func deleteAction(context *cli.Context) {
-	var (
-		appFile  = context.Args().Get(0)
-		hostName = context.Args().Get(1)
-	)
-
-	app := runTrans(appFile, hostName, "DELETE", "app")
-
-	if err := registry.DeleteApplication(app.ID); err != nil {
+	app, err := loadApp(context)
+	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	for _, hn := range context.StringSlice("hosts") {
+		if err := runTrans(app, hn, "DELETE", "app"); err != nil {
+			logger.WithField("error", err).Errorf("deleting app from %s", hn)
+		}
+	}
+
+	if err := registry.DeleteApplication(app.ID); err != nil {
+		logger.WithField("error", err).Fatal("delete application from registry")
 	}
 }
 
 func loadAction(context *cli.Context) {
-	var (
-		appFile = context.Args().Get(0)
-	)
-
-	app, err := loadApp(appFile)
+	app, err := loadApp(context)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -82,29 +91,41 @@ func loadAction(context *cli.Context) {
 	}
 
 	for _, hn := range context.StringSlice("hosts") {
-		runTrans(appFile, hn, "POST", "app")
+		if err := runTrans(app, hn, "POST", "app"); err != nil {
+			logger.WithField("error", err).Errorf("cannot load app on %s", hn)
+		}
 	}
 }
 
 func startAction(context *cli.Context) {
-	var (
-		appFile  = context.Args().Get(0)
-		hostName = context.Args().Get(1)
-	)
+	app, err := loadApp(context)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-	runTrans(appFile, hostName, "POST", "run")
+	for _, hn := range context.StringSlice("hosts") {
+		if err := runTrans(app, hn, "POST", "run"); err != nil {
+			logger.WithField("error", err).Errorf("cannot run container on %s", hn)
+		}
+	}
 }
 
 func stopAction(context *cli.Context) {
-	var (
-		appFile  = context.Args().Get(0)
-		hostName = context.Args().Get(1)
-	)
+	app, err := loadApp(context)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-	runTrans(appFile, hostName, "POST", "stop")
+	for _, hn := range context.StringSlice("hosts") {
+		if err := runTrans(app, hn, "POST", "stop"); err != nil {
+			logger.WithField("error", err).Errorf("cannot stop container on %s", hn)
+		}
+	}
 }
 
-func runTrans(appFile, hostName, method, endpoint string) *citadel.Application {
+func runTrans(app *citadel.Application, hostName, method, endpoint string) error {
 	var (
 		c = http.Client{
 			Transport: &http.Transport{
@@ -113,54 +134,42 @@ func runTrans(appFile, hostName, method, endpoint string) *citadel.Application {
 		}
 	)
 
-	app, err := loadApp(appFile)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
 	host, err := registry.FetchHost(hostName)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	url := fmt.Sprintf("https://%s/%s/%s", host.Addr, endpoint, app.ID)
 
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	resp, err := c.Do(req)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	var tran *citadel.Transaction
 	if err := json.NewDecoder(resp.Body).Decode(&tran); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	compiled, err := template.New("transaction").Parse(tranactionTemplate)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
 	if err := compiled.Execute(os.Stdout, tran); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 
-	return app
+	return nil
 }
 
-func loadApp(p string) (*citadel.Application, error) {
-	f, err := os.Open(p)
+func loadApp(context *cli.Context) (*citadel.Application, error) {
+	f, err := os.Open(context.Args().First())
 	if err != nil {
 		return nil, err
 	}
