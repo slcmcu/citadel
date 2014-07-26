85706a7c35000c08eed0e57090a885cbc800fed7
Michael Crosby <michael@docker.com>
2014-07-26 15:38:17 -0700

    Add label scheduler

--- container.go
+++ container.go
@@ -1,8 +1,9 @@
 package citadel
 
 type Container struct {
-	Image  string  `json:"image,omitempty"`
-	Cpus   float64 `json:"cpus,omitempty"`
-	Memory float64 `json:"memory,omitempty"`
-	Type   string  `json:"type,omitempty"`
+	Image  string   `json:"image,omitempty"`
+	Cpus   float64  `json:"cpus,omitempty"`
+	Memory float64  `json:"memory,omitempty"`
+	Type   string   `json:"type,omitempty"`
+	Labels []string `json:"labels,omitempty"`
 }
---
+++ label_scheduler.go
@@ -0,0 +1,44 @@
+package citadel
+
+type LabelScheduler struct {
+}
+
+func (l *LabelScheduler) Schedule(resources []*Resource, c *Container) ([]*Resource, error) {
+	if len(c.Labels) == 0 {
+		return resources, nil
+	}
+
+	out := []*Resource{}
+
+	for _, r := range resources {
+		if l.contains(r, c.Labels) {
+			out = append(out, r)
+		}
+	}
+
+	if len(out) == 0 {
+		return nil, ErrUnableToSchedule
+	}
+
+	return out, nil
+}
+
+func (l *LabelScheduler) contains(r *Resource, constraints []string) bool {
+	for _, c := range constraints {
+		if !l.resourceContains(r, c) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (l *LabelScheduler) resourceContains(r *Resource, c string) bool {
+	for _, l := range r.Labels {
+		if l == c {
+			return true
+		}
+	}
+
+	return false
+}
