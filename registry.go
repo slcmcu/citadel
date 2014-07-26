bca7ece1eb8577535165e71b4203fab281f1a132
Michael Crosby <michael@docker.com>
2014-07-26 14:45:50 -0700

    Add more registry methods

--- registry.go
+++ registry.go
@@ -1,9 +1,15 @@
 package citadel
 
 type Registry interface {
+	// FetchResources returns all the resources registered in the cluster
+	FetchResources() ([]*Resource, error)
+
 	// GetTotalReservations returns the total cpu and memory reserved for the resource by id
 	GetTotalReservations(id string) (cpus float64, memory float64, err error)
 
 	// PlaceReservation ensures that the resource has the container's reservation locked
 	PlaceReservation(string, *Container) error
+
+	// ReleaseReservation releases any resource allocations for the resource
+	ReleaseReservation(string, *Container) error
 }
--- resource_manager_test.go
+++ resource_manager_test.go
@@ -1,52 +1,6 @@
 package citadel
 
-import (
-	"fmt"
-	"testing"
-)
-
-type mockRegistry struct {
-	resources map[string]*reservation
-}
-
-type reservation struct {
-	r      *Resource
-	cpus   float64
-	memory float64
-}
-
-func newMockRegistry(rs []*Resource) *mockRegistry {
-	m := &mockRegistry{
-		resources: make(map[string]*reservation, len(rs)),
-	}
-
-	for _, r := range rs {
-		m.resources[r.ID] = &reservation{r: r}
-	}
-
-	return m
-}
-
-func (r *mockRegistry) GetTotalReservations(id string) (float64, float64, error) {
-	rs := r.resources[id]
-	if rs == nil {
-		return 0, 0, fmt.Errorf("no resource for id %s", id)
-	}
-
-	return rs.cpus, rs.memory, nil
-}
-
-func (r *mockRegistry) PlaceReservation(id string, c *Container) error {
-	re := r.resources[id]
-	if re == nil {
-		return fmt.Errorf("no resource for id %s", id)
-	}
-
-	re.cpus += c.Cpus
-	re.memory += c.Memory
-
-	return nil
-}
+import "testing"
 
 func TestScheduleHighMemory(t *testing.T) {
 	resources := []*Resource{
