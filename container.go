75a6fefcafecc23bd26c18becc9291abd39dff71
Evan Hazlett <ejhazlett@gmail.com>
2014-07-30 14:50:27 -0400

    add omitempty for all json struct tags

--- container.go
+++ container.go
@@ -1,13 +1,13 @@
 package citadel
 
 type Container struct {
-	Name        string            `json:"name"`
+	Name        string            `json:"name,omitempty"`
 	Image       string            `json:"image,omitempty"`
 	Cpus        float64           `json:"cpus,omitempty"`
 	Memory      float64           `json:"memory,omitempty"`
-	Environment map[string]string `json:"environment"`
-	Hostname    string            `json:"hostname"`
-	Domainname  string            `json:"domain"`
+	Environment map[string]string `json:"environment,omitempty"`
+	Hostname    string            `json:"hostname,omitempty"`
+	Domainname  string            `json:"domain,omitempty"`
 	Type        string            `json:"type,omitempty"`
 	Labels      []string          `json:"labels,omitempty"`
 }
