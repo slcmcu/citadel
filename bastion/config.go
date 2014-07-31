66b1e0cacfd2aedd9232445397cd36eeaab4144e
Michael Crosby <michael@docker.com>
2014-07-30 22:15:51 -0700

    Remove redis config fields

--- bastion/config.go
+++ bastion/config.go
@@ -11,8 +11,6 @@ type Config struct {
 	SSLCertificate string            `json:"ssl-cert,omitempty"`
 	SSLKey         string            `json:"ssl-key,omitempty"`
 	CACertificate  string            `json:"ca-cert,omitempty"`
-	RedisAddr      string            `json:"redis-addr,omitempty"`
-	RedisPass      string            `json:"redis-pass,omitempty"`
 	ListenAddr     string            `json:"listen-addr,omitempty"`
 	Dockers        []*citadel.Docker `json:"dockers,omitempty"`
 }
