732356d24ebb655285b89e13b0446239cefb1a5a
Evan Hazlett <ejhazlett@gmail.com>
2014-08-05 00:30:13 -0400

    revert me as jackass

--- scheduler/host_scheduler.go
+++ scheduler/host_scheduler.go
@@ -19,10 +19,6 @@ func (h *HostScheduler) Schedule(c *citadel.Image, e *citadel.Engine) (bool, err
 }
 
 func (h *HostScheduler) validHost(e *citadel.Engine, labels []string) bool {
-	// randomize lookup
-	rand.Seed(time.Now().UTC().UnixNano())
-	i := rand.Intn(len(labels) - 1)
-	label := labels[i]
 	parts := strings.Split(label, "host:")
 	if len(parts) != 2 {
 		return false
